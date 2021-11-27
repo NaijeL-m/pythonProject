c33cb73bb6ef1363119afea1ce8eb6ae12ae8c78
NaijeL-m <brvanechka@yandex.ru>
2021-11-27 15:12:03 +0500

    Add

--- main.py
+++ main.py
@@ -3,6 +3,43 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
+import yadisk
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def ya_headers(self):
+        return {
+            'Content-type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def get_link_ya(self,adres_disk_file):
+        url="https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers=self.ya_headers()
+        params={"path":adres_disk_file}
+        response=requests.get(url, headers=headers, params=params)
+        return response.json()
+
+    def upload(self, file_name):
+        href=self.get_link_ya(adres_disk_file="test/"+file_name).get("href", "")
+        print(href)
+        response=requests.put(href, data=open(file_name, 'rb'))
+        response.raise_for_status()
+        return response.status_code
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        # Функция может ничего не возвращать
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file_on_computer = "test.txt"
+    token = "AQAAAABXhZfHAADLW0t9sCIlwkPlnOFc_Q_3CBM"
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file_on_computer)
+    #result = uploader.get_link_ya("test/net").get("href", "")
+    print(result)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -10,8 +47,6 @@ def print_hi(name):
 
 a=0
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 x=requests.get(' https://superheroapi.com/api/2619421814940190/search/Hulk')
 slovar= x.json()
